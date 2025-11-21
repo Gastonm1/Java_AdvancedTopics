3be63f7d98a00d1e5ed371cd896f4170f69110b2
Marques <gaston.marques@gmail.com>
2025-11-21 14:37:19 -0500

    Part 3 - Advanced Topics - The finally block

--- src/exceptions/ExceptionsDemo.java
+++ src/exceptions/ExceptionsDemo.java
@@ -8,17 +8,22 @@ import java.text.SimpleDateFormat;
 
 public class ExceptionsDemo {
     public static void show() {
+        FileReader reader = null;
         try {
-           var reader = new FileReader("file.txt");
+            reader = new FileReader("file.txt");
             var value = reader.read();
-            new SimpleDateFormat().parse("");
-        } catch (IOException | ParseException e) {
+        } catch (IOException e) {
             System.out.println("Could not read data");
         }
-
-    }
-
-    public static void sayHello(String name) {
-        System.out.println(name.toUpperCase());
+        // The final block will always get executed, whether you have an exception or not
+        finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 }

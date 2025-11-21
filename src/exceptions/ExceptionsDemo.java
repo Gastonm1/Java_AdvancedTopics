44be2834426a1e1cd40f26cc7c3a74ce3584cba3
Marques <gaston.marques@gmail.com>
2025-11-21 14:45:39 -0500

    Part 3 - Advanced Topics - The try-with-resources statement

--- src/exceptions/ExceptionsDemo.java
+++ src/exceptions/ExceptionsDemo.java
@@ -1,29 +1,17 @@
 package exceptions;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 public class ExceptionsDemo {
     public static void show() {
-        FileReader reader = null;
-        try {
-            reader = new FileReader("file.txt");
+        try (var reader = new FileReader("file.txt");
+        var writer = new FileWriter("...");
+        ){
             var value = reader.read();
         } catch (IOException e) {
             System.out.println("Could not read data");
-        }
-        // The final block will always get executed, whether you have an exception or not
-        finally {
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
         }
     }
 }

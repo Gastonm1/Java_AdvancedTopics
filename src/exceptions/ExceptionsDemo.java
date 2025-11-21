c9388172819dcdc7414a4e5a2dcc3401f09d085e
Marques <gaston.marques@gmail.com>
2025-11-21 14:29:35 -0500

    Part 3 - Advanced Topics - Catching Multiple Types of Exceptions

--- src/exceptions/ExceptionsDemo.java
+++ src/exceptions/ExceptionsDemo.java
@@ -2,14 +2,18 @@ package exceptions;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class ExceptionsDemo {
     public static void show() {
         try {
            var reader = new FileReader("file.txt");
-            System.out.println("File Opened");
-        } catch(FileNotFoundException ex) {
-            System.out.println(ex.getMessage());
+            var value = reader.read();
+            new SimpleDateFormat().parse("");
+        } catch (IOException | ParseException e) {
+            System.out.println("Could not read data");
         }
 
     }

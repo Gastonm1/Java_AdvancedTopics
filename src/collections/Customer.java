7e49a5b7e2bb232f1cbc8f72b8fe30baa0823b17
Marques <gaston.marques@gmail.com>
2025-11-25 12:18:13 -0500

    Part 3 - Advanced Topics - The Comparator Interface

--- src/collections/Customer.java
+++ src/collections/Customer.java
@@ -4,9 +4,11 @@ package collections;
 
 public class Customer implements Comparable<Customer> {
     private String name;
+    private String email;
 
-    public Customer(String name) {
+    public Customer(String name, String email) {
         this.name = name;
+        this.email = email;
     }
 
     @Override
@@ -17,5 +19,13 @@ public class Customer implements Comparable<Customer> {
     @Override
     public String toString() {
         return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }

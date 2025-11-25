ea3273798ceb2f6352ee8f12da4a5e31412730d4
Marques <gaston.marques@gmail.com>
2025-11-25 14:38:27 -0500

    Part 3 - Advanced Topics - FLambda Expressions

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 import lambdas.LambdasDemo;
 
 public class Main {
-    // What is a functional interface?
-    // An interface with a single abstract method
-
     public static void main(String[] args) {
         LambdasDemo.show();
     }
--- src/lambdas/LambdasDemo.java
+++ src/lambdas/LambdasDemo.java
@@ -1,13 +1,25 @@
 package lambdas;
 
+// What is a Lambda Expression?
+// Lambda expression is an anonymous function that we can pass around
+// A function that exists on its own without belonging to a class
+
 public class LambdasDemo {
     public static void show() {
-    greet(new Printer() {
-        @Override
-        public void print(String message) {
+//    greet(new Printer() {
+//        @Override
+//        public void print(String message) {
+//            System.out.println(message);
+//        }
+//    });
+
+    // rewriting the above as a lambda expression
+        greet((message) -> {
             System.out.println(message);
-        }
-    });
+        });
+        // You can also store lamba expressions in a variable
+        Printer printer = message -> System.out.println(message);
+
     }
 
     public static void greet(Printer printer) {

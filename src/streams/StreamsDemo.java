e924170790ece0a341520e6f1bc2fa1993645f91
Marques <gaston.marques@gmail.com>
2025-11-26 16:21:26 -0500

    Part 3 - Advanced Topics -  Grouping Elements

--- src/streams/StreamsDemo.java
+++ src/streams/StreamsDemo.java
@@ -6,25 +6,27 @@ import java.util.stream.Collectors;
 public class StreamsDemo {
     public static void show() {
         var movies = List.of(
-                new Movie("a", 10),
-                new Movie("b", 20),
-                new Movie("c", 30)
+                new Movie("a", 10, Genre.THRILLER),
+                new Movie("b", 20, Genre.ACTION),
+                new Movie("c", 30, Genre.ACTION)
         );
 
-        // Collectors class (https://docs.oracle.com/javase/8/docs/api/java/util/stream/Collectors.html)
-        // The Collector implements various useful reduction operations
-        // accumulating elements into collections
-        // summarizing elements according to various criteria, etc.
-        // EX:
-        // toList() -> Returns a Collector that accumulates the input elements into a new List
-        // toSet() -> Returns a Collector that accumulates the input elements into a new Set
-        // toMap() -> Returns a Collector that accumulates elements into a Map whose keys and values are the result of applying the provided mapping functions to the input elements.
+        // Sometimes you may want to group or classify the data
+        // Ex: classify the movies by genre.
         var result = movies.stream()
-                .filter(m -> m.getLikes() > 10)
-//                .collect(Collectors.toList());
-//                .collect(Collectors.toSet());
-                  .collect(Collectors.toMap(m -> m.getTitle(), m -> m.getLikes()));
+                // Separated into objects by genre
+//                .collect(Collectors.groupingBy(
+//                        Movie::getGenre));
 
+                // Number of objects OF each Genre
+                //  .collect(Collectors.groupingBy(
+                //   Movie::getGenre, Collectors.counting()));
+
+                // Join name of movies with comma (CAN ONLY JOIN WITH STRINGS)
+                // So get the movie title FIRST and then join
+                .collect(Collectors.groupingBy(
+                        Movie::getGenre,
+                        Collectors.mapping(Movie::getTitle, Collectors.joining(","))));
         System.out.println(result);
     }
 }

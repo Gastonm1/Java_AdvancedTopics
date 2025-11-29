3e44583e6b96d68510254ab53270ee473af93575
Marques <gaston.marques@gmail.com>
2025-11-29 09:31:14 -0500

    Part 3 - Advanced Topics - Pausing a thread

--- src/concurrency/DownloadFileTask.java
+++ src/concurrency/DownloadFileTask.java
@@ -3,6 +3,18 @@ package concurrency;
 public class DownloadFileTask implements Runnable {
     @Override
     public void run() {
-        System.out.println("Downloading a file: " + Thread.currentThread().getName());
+        System.out.println("Starting file download: " + Thread.currentThread().getName());
+
+        // Thread.sleep causes the current thread to suspend execution for a specified period.
+        // This is an efficient means of making processor time available to the other threads of an application or other applications that might be running on a computer system.
+        // .sleep() takes one argument which is how long you want to suspend the execution: .sleep(long millis)
+        // below example is 5 seconds
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Download complete :" + Thread.currentThread().getName());
     }
 }

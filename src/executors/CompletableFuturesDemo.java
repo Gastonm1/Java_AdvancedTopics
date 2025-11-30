f6cb3f4173acb70c43d1649a9c00075b1b1b8e02
Marques <gaston.marques@gmail.com>
2025-11-30 12:41:46 -0500

    Part 3 - Transforming a Completable Future

--- src/executors/CompletableFuturesDemo.java
+++ src/executors/CompletableFuturesDemo.java
@@ -1,29 +1,19 @@
 package executors;
+
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
+// .thenApply(): Returns a new CompletionStage that, when this stage completes normally
+// is executed with this stage's result as the argument to the supplied function.
+// .thenApplyAsync: same thing but does it asynchronously
 public class CompletableFuturesDemo {
-    public static void show(){
-        var future = CompletableFuture.supplyAsync(() -> {
-            System.out.println("Getting the current weather");
-            throw new IllegalStateException();
-        });
+    public static int toFahrenheit(int celsius){
+        return (int) (celsius * 1.8) + 32;
+    }
 
-        // Exception Handling
-        // .exceptionally(): Lets you recover from an exception inside a CompletableFuture. It catches the error and lets you return a fallback value.
-        // "If something wrong, do this instead"
-        // This way, the future WILL NOT fail - it returns the fallback.
-        // InterruptedException: The thread was told to stop what its doing and return early.
-        // "You're doing something, but someone tapped your shoulder saying stop"
-        // ExecutionException: Something inside the async task threw an exception - this wraps it.
-        // does not tell you the real error directly. It's a wrapper containing the real exception
-            try {
-                var temperature = future.exceptionally(ex -> 1).get();
-                System.out.println(temperature);
-            } catch (InterruptedException e){
-                e.printStackTrace();
-            } catch (ExecutionException e){
-                e.printStackTrace();
-            }
+    public static void show(){
+     var future = CompletableFuture.supplyAsync(() -> 20);
+        var result = future
+                .thenApply(CompletableFuturesDemo::toFahrenheit)
+                .thenAccept(f -> System.out.println(f));
     };
 }

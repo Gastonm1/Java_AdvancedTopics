30284f87f88778d030c06314e24d87df15c8577b
Marques <gaston.marques@gmail.com>
2025-11-30 07:24:19 -0500

    Part 3 - Advanced Topics - Callables and Futures

--- src/executors/ExecutorsDemo.java
+++ src/executors/ExecutorsDemo.java
@@ -1,34 +1,31 @@
 package executors;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 
-// The Executors class has a number of static factory methods for constructing executor service instances.
-// newCachedThreadPool: New threads are created as needed; idle threads are kept for 60 seconds
-// newFixedThreadPool: The pool contains a fixed set of threads; idle threads are kept indefinitely
-// newSingleThreadExecutor: A "pool" with a single thread that executes the submitted tasks sequentially
-// newVirtualThreadPerTaskExecutor: An executor that runs each task on a new virtual thread
+// The Callable interface in Java, found within the java.util.concurrent package, represents a task that returns a result and may throw an exception.
+// It is designed for use in multithreaded environments, particularly with the Java Concurrency API, as an alternative to the Runnable interface.
+// Very similar to the runnable interface but this represents a task that returns a value
 
 public class ExecutorsDemo {
     public static void show() {
         var executor = Executors.newFixedThreadPool(2);
 
-        // When you're working with executors. After it completes a task, it keeps running in case there might be more tasks coming in the future.
-        // So it's not going to terminate, it's going to stay in the memory, waiting for new tasks
-        // So we have to explicitly shut down an executor and terminate our program.
-        // We can use .shutdown() or .shutdownNow()
-        // .shutdown(): will wait to the task its working on is complete and then shutdown
-        // .shutdownNow(): will forcefully stop the task
-        // Make sure to put it in a try block
-
         try {
-        executor.submit(() -> {
-            System.out.println(Thread.currentThread().getName());
-        });
+        var future = executor.submit(() -> {
+                LongTask.simulate();
+                return 1;
+            });
+            System.out.println("Do more work");
+            try {
+                var result = future.get();
+                System.out.println(result);
+            } catch (InterruptedException | ExecutionException  e) {
+                e.printStackTrace();
+            }
         }
         finally {
             executor.shutdown();
         }
-
-
     }
 }

affc557d5a8b44537016c863ae7d9e55b7b11754
Marques <gaston.marques@gmail.com>
2025-11-30 13:01:58 -0500

    Part 3 - Waiting for the First Task

--- src/executors/CompletableFuturesDemo.java
+++ src/executors/CompletableFuturesDemo.java
@@ -1,26 +1,18 @@
 package executors;
 
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 public class CompletableFuturesDemo {
        public static void show(){
-        var first = CompletableFuture.supplyAsync(() -> 1);
-        var second = CompletableFuture.supplyAsync(() -> 2);
-        var third = CompletableFuture.supplyAsync(() -> 3);
-
-        var all = CompletableFuture.allOf(first,second,third);
-        all.thenRun(() -> {
-            try {
-                var firstResult = first.get();
-                System.out.println(firstResult);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            } catch (ExecutionException e) {
-                throw new RuntimeException(e);
-            }
-            System.out.println("All tasks completed successfully");
+        var first = CompletableFuture.supplyAsync(() -> {
+            LongTask.simulate();
+            return 20;
         });
 
+        var second = CompletableFuture.supplyAsync(() -> 20);
+
+        // .anyOf: immediately returns a future that completes when the ready regardless if there are several other futures completes — success or failure.
+        CompletableFuture.anyOf(first, second)
+                .thenAccept(temp -> System.out.println(temp));
     };
 }

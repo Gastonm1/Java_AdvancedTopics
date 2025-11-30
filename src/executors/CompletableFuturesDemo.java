04b55716884c66c52f54fc011eac12b63037f102
Marques <gaston.marques@gmail.com>
2025-11-30 12:48:36 -0500

    Part 3 - Combining Completable Futures

--- src/executors/CompletableFuturesDemo.java
+++ src/executors/CompletableFuturesDemo.java
@@ -2,18 +2,21 @@ package executors;
 
 import java.util.concurrent.CompletableFuture;
 
-// .thenApply(): Returns a new CompletionStage that, when this stage completes normally
-// is executed with this stage's result as the argument to the supplied function.
-// .thenApplyAsync: same thing but does it asynchronously
 public class CompletableFuturesDemo {
-    public static int toFahrenheit(int celsius){
-        return (int) (celsius * 1.8) + 32;
+    public static CompletableFuture<String> getUserEmailAsync() {
+        return CompletableFuture.supplyAsync(() -> "email");
+    }
+
+    public static CompletableFuture<String> getPlaylistAsync(String email) {
+        return CompletableFuture.supplyAsync(() -> "playlist");
     }
 
     public static void show(){
-     var future = CompletableFuture.supplyAsync(() -> 20);
-        var result = future
-                .thenApply(CompletableFuturesDemo::toFahrenheit)
-                .thenAccept(f -> System.out.println(f));
+     // id -> email
+        getUserEmailAsync()
+        // email -> playlist
+        .thenCompose(CompletableFuturesDemo::getPlaylistAsync)
+        .thenAccept(playlist -> System.out.println(playlist));
+
     };
 }
